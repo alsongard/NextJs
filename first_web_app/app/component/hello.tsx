310da3779b286cf52af6081e22905e1649b05600
alsongard <alsongard@gmail.com>
2025-01-07 03:46:28 +0300

    client side rendering using 'use client'; directive

---
+++ first_web_app/app/component/hello.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+
+export default function Hello() {
+    console.log("I am a client comoonent");
+    return (
+        <h1 className="text-[30px] text-center">Hello from the Client Component</h1>
+    )
+}
