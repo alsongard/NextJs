e6e013dc3c6c717fca5a9fd55c9efef7d2f75df2
alsongard <alsongard@gmail.com>
2025-01-07 03:47:30 +0300

    setting up homepage

--- first_web_app/app/page.tsx
+++ first_web_app/app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import Hello from "./component/hello";
 
 export default function Home() {
+  console.log("What i am i doing here ? --Server/Client"); 
   return (
-    <h1 className="text-[30px] text-center">Welcome to Nextjs</h1>
+    <div>
+      <h1 className="text-[30px] text-center">Welcome to Nextjs</h1>
+      <Hello />
+    </div>
   );
 }
