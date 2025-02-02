6da378ce3435d91ade1fcd6b5214cfe9435dd773
alsongard <alsongard@gmail.com>
2025-02-02 20:35:40 +0200

    added navbar for webpage

--- NextJS13/next01/app/layout.tsx
+++ NextJS13/next01/app/layout.tsx
@@ -12,7 +12,12 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <nav>
+          <h1>Hello there am Main Navbar</h1>
+        </nav>
+        {children}
+        </body>
     </html>
   )
 }
