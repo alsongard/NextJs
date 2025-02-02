9af9fd1496cf9dba3d8280cebc12925aaa999202
alsongard <alsongard@gmail.com>
2025-02-02 21:50:47 +0200

    added metadata info for aboutpage >>> very awesome stuff

--- NextJS13/next01/app/about/layout.tsx
+++ NextJS13/next01/app/about/layout.tsx
@@ -1,14 +1,27 @@
+import styles from './styles.module.css'
 import Link from 'next/link'
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'About Page',
+  description: 'my about page',
+}
+ 
+
+
 export default  function AboutLayout({
     children,
   }: {
     children: React.ReactNode
   }) {
     return (
-    <main>
-        <h2> Hi there am a About layout</h2>
-        {children}  {/**this is the page.tsx for the about page */}
-        <Link href="/">Click to go to  home page</Link>
-    </main>
+        <div>
+        <main className={styles.main} >
+            <h2> Hi there am a About layout</h2>
+            {children}  {/**this is the page.tsx for the about page */}
+            <Link href="/">Click to go to  home page</Link>
+        </main>
+        </div>
   )
 }
+
