2bb27c444ab7037c535d2fe930d06b4b96078acc
alsongard <alsongard@gmail.com>
2025-02-18 01:53:53 +0200

    Users page which getsAllUsers and display using map

---
+++ NextJS13/next03/app/users/page.tsx
@@ -0,0 +1,42 @@
+import type {Metadata} from "next";
+import getAllUsers from "@/lib/getAllUsers";
+import Link  from "next/link";
+export const metadata : Metadata = {
+    title : "Users",
+}
+
+
+export default async function UserPage() {
+  const usersData: Promise<User[]> = getAllUsers()
+  const users = await usersData;
+  console.log(`Type of users is : ${typeof(users)}`);
+
+  const userContent = users.map((userItem)=>{
+    return (
+      <div>
+        <p key={userItem.id}> <Link href={`/users/${userItem.id}`}> {userItem.name}</Link></p>
+      </div>
+    )
+  })
+  const myElement = (
+      <div>
+
+        <br/>
+        {users.map(user => {
+          return (
+            <>
+              <p key={user.id}>
+                <Link href={`/users/${user.id}`}>{user.name}</Link>
+              </p>
+            </>
+          )
+        })}
+      </div>
+    )
+  return (
+    <section>
+      <h2><Link href="/">Back home</Link></h2>
+      {userContent}
+    </section>
+  )
+}
