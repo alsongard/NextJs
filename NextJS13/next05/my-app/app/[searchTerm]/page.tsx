d0dc7429063130ae280f26a827adb7b8daf5a4a8
alsongard <alsongard@gmail.com>
2025-03-23 21:30:37 +0300

    searchPage working

--- NextJS13/next05/my-app/app/[searchTerm]/page.tsx
+++ NextJS13/next05/my-app/app/[searchTerm]/page.tsx
@@ -1,23 +1,73 @@
 import WikiSearch from "@/lib/getWikiResult";
-
+import { title } from "process";
+import React from "react";
 type Props = {
     params : {
         searchTerm: string
     }
 }
 
+
+// generate Metadata 
+export  async function generateMetadata({params: {searchTerm}}:Props)
+{
+    const wikiData: Promise<SearchResult>  = WikiSearch(searchTerm);
+    const data = await wikiData;
+    // fetch data(nextjs will deduplicate (not fetch again))
+
+    const search = searchTerm.replaceAll("%20", " ")
+    if (!data?.query?.pages)
+    {
+        return {
+            title: `No  results for ${search}`,
+            description: `No results found for ${searchTerm}`
+        }
+    }
+
+    
+    return {
+        title: `${searchTerm}`,
+        description: `The results for ${searchTerm} are shown below.`
+    }
+}
+
+
+
 export default async function SearchTerm({params : {searchTerm}} : Props)
 {
     const wikiData: Promise<SearchResult>  = WikiSearch(searchTerm);
     console.log(`the searchTerm is : ${searchTerm}`);
     const data = await wikiData;
-    const result:Result[] | undefined = data?.query?.pages;
+    const results:Result[] | undefined = data?.query?.pages;
 
-    console.log(result);
-    return (
-        <div>
-            <p>Getting started</p>
-        </div>
+    const search = searchTerm.replaceAll("%20", " ")
+
+
+    console.log("this are the results: ");
+    // console.log(result);
+
+    // variable for conditional rendering // using tenary statement
+    let beViewed = true;
+    if (results == undefined)
+    {
+        console.log("undefined data");
+        beViewed = false;
+
+    }
+    const content = (
+           <div>
+                {results 
+                    ?
+                        Object.values(results).map((result) => {
+                            return <p>{JSON.stringify(result)}</p>
+                        })
+                    :
+                        <h2>The term {search} cannot be found</h2>
+                }
+           </div>
     )
+    // const retreived_data = results ? results : undefined
+    // console.log(retreived_data);
+    return content
 
 }
