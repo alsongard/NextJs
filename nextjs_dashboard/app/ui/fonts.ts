6a5db709cfd7b88f779b3676438f96da306bf7a6
alsongard <alsongard@gmail.com>
2025-10-13 12:20:27 +0300

    using fonts: optimization on fonts

---
+++ nextjs_dashboard/app/ui/fonts.ts
@@ -0,0 +1,6 @@
+//  you will use this file to use all youor fonts:for optimization
+
+import {Inter, Lusitana} from "next/font/google";
+
+export const inter =  Inter({subsets: ['latin']});
+export const lusitana = Lusitana({weight: ['400', '700'],  subsets: ['latin']});
