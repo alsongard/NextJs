3aa9b22ffd27031e0f4fb4527cad620e471df385
alsongard <alsongard@gmail.com>
2025-10-17 17:57:23 +0300

    search component update:synchronizationUrl&SearchParamsSuccess

--- nextjs_dashboard/app/ui/search.tsx
+++ nextjs_dashboard/app/ui/search.tsx
@@ -1,18 +1,57 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, useRouter, usePathname } from 'next/navigation';
 
-export default function Search({ placeholder }: { placeholder: string }) {
-  return (
-    <div className="relative flex flex-1 flex-shrink-0">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
-      <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
-      />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-    </div>
-  );
+import {useDebouncedCallback} from 'use-debounce'
+
+
+export default function Search({ placeholder }: { placeholder: string })
+{
+	// create a instance of useSearchParams()
+	const searchParams = useSearchParams();
+
+	const {replace} = useRouter(); // for programatic navigation
+
+	const pathName = usePathname(); // getting the current WEBPAGE URL
+
+	// create a new URLSearchParams instance using your new searchParams variable
+	// The URLSearchParams interface defines utility methods to work with the query string of a URL.
+	const params = new URLSearchParams(searchParams);
+	const handleSearch = useDebouncedCallback((term:string)=>{
+		console.log('input changed');
+		console.log(term);
+		console.log(`params`);
+		console.log(params);
+
+		if (term)
+		{
+			params.set('query', term);
+			params.set('page', '1');
+			console.log(`params after set`);
+			console.log(params);
+			console.log('params.toString');
+			console.log(params.toString())
+		}
+		else
+		{
+			params.set('query', "");
+		}
+		replace(`${pathName}?${params.toString()}`)
+	}, 2000);
+		
+	return (
+		<div className="relative flex flex-1 flex-shrink-0">
+		<label htmlFor="search" className="sr-only">
+			Search
+		</label>
+		<input
+			className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+			placeholder={placeholder}
+			onChange={(e)=>handleSearch(e.target.value)}
+			defaultValue={searchParams.get('query')?.toString()}
+		/>
+		<MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+		</div>
+	);
 }
